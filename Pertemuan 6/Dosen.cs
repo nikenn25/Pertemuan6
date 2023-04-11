be027d82347efcb3ad2c5975031c71c24bd043f3
nikenn25 <130416119+nikenn25@users.noreply.github.com>
2023-04-11 08:37:29 +0700

    Add files via upload

---
+++ Pertemuan 6/Dosen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pertemuan_6
+{
+    class Dosen
+    {
+        private int id;
+        private string nama;
+        private int Nik;
+        private string gender;
+        private string course;
+        public Dosen() {
+            id = 1;
+
+            nama = "Dosen Pengampu";
+            Nik = 123456789;
+            gender = "Male";
+            course = "Pemrograman Berorientasi Objek";
+        }
+
+        public void attDosen(string name, int nik)
+        {
+            nama = name;
+            Nik = nik;
+        }
+
+        public void addCourse(string newCourse)
+        {
+            course += ", " + newCourse;
+        }
+        public void display()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Nama: " + nama);
+            Console.WriteLine("Nik: " + Nik);
+            Console.WriteLine("Gender: " + gender);
+            Console.WriteLine("Course: " + course);
+
+        }
+    }
+}
---
+++ Pertemuan 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using Pertemuan_6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+            
+            Console.WriteLine("Default Dosen Pengampu");
+            dosen.display();
+
+            Console.WriteLine("\nMasukan Nama dan NIK Dosen Baru");
+            dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+            dosen.display();
+
+            Console.WriteLine("\nMasukan Mata Kuliah Baru");
+            dosen.addCourse(Console.ReadLine());
+            dosen.display();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
